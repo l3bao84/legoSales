f1ec525eb1c4d518596424ad058a0025c18ba1f4
l3bao84 <l3ducb4008@gmail.com>
2023-08-15 00:09:49 +0700

    a little bit change(15/08/2023)

--- sales/src/main/java/com/LeBao/sales/controllers/ProductController.java
+++ sales/src/main/java/com/LeBao/sales/controllers/ProductController.java
@@ -1,6 +1,9 @@
 package com.LeBao.sales.controllers;
 
+import com.LeBao.sales.models.Category;
 import com.LeBao.sales.models.Product;
+import com.LeBao.sales.repositories.CategoryRepository;
+import com.LeBao.sales.repositories.ProductRepository;
 import com.LeBao.sales.services.ProductService;
 import com.LeBao.sales.services.StorageService;
 import lombok.RequiredArgsConstructor;
@@ -9,10 +12,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/product")
@@ -23,15 +31,48 @@ public class ProductController {
     private ProductService productService;
 
     @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
     private StorageService storageService;
 
     @GetMapping("/getProduct/{productId}")
-    public ResponseEntity<Product> getProduct(@PathVariable Long productId) {
-        if(productService.getProduct(productId) == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    public String getProduct(ModelMap modelMap, @PathVariable Long productId) {
+        List<Product> products = productRepository.findAll();
+        Collections.reverse(products);
+        List<Product> recommendedProducts = new ArrayList<>();
+        List<Product> topPickProducts = new ArrayList<>();
+        modelMap.addAttribute("allThemes", categoryRepository.findAll());
+        List<Category> allCats = new ArrayList<>();
+        allCats = categoryRepository.findAll();
+
+        for (int i = 0; i < 12; i++) {
+            recommendedProducts.add(products.get(i));
         }
-        return ResponseEntity.status(HttpStatus.FOUND).body(productService.getProduct(productId));
+
+        topPickProducts.add(productRepository.findById(1L).get());
+        topPickProducts.add(productRepository.findById(14L).get());
+        topPickProducts.add(productRepository.findById(15L).get());
+        topPickProducts.add(productRepository.findById(6L).get());
+
+        allCats.remove(0);
+        List<Category> randomCat = new ArrayList<>();
+        Random random = new Random();
+        for(int i = 0; i <= 3; i++) {
+            int randomPosition = random.nextInt(allCats.size());
+            randomCat.add(allCats.get(randomPosition));
+            allCats.remove(randomPosition);
+        }
+        modelMap.addAttribute("products", recommendedProducts);
+        modelMap.addAttribute("topPickProducts", topPickProducts);
+        modelMap.addAttribute("randomCat", randomCat);
+        modelMap.addAttribute("productName", productRepository.findById(productId).get().getProductName());
+        return "detailProduct";
     }
+
 
     @PostMapping("/addProduct")
     public ResponseEntity<String> addProduct(@RequestBody Product product) {
